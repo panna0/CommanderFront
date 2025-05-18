8c211dcf911f212797e974759c04342f71413a85
Panna0 <arianna.ferri2004@gmail.com>
2025-05-18 15:42:43 +0200

    add button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import styles from "./Button.module.scss"
 
-const Button = ({children, type}) => {
+const Button = ({children, color, size, action}) => {
     
     //const {children} = props;
     //const typeClass = type === "primary" ? "btn-primary" : "btn-secondary";
-    const typeClass = (type === "primary" || type === "secondary") 
-        ? styles["btn-primary"] 
-        : styles["btn-secondary"];
+   
+
+    const colorClass = color === "primary"
+    ? styles["btn-primary"]
+    : color === "secondary"
+        ? styles["btn-secondary"]
+        : "";
+
+
+    const sizeClass = size === "small"
+    ? styles["btn-small"]
+    : size === "large"
+        ? styles["btn-large"]
+        : "";
 
     return(
 
-       <button className={`${styles.btn} ${typeClass}`}>{children}</button> //children.props o const {children} = a props e solo {children}
+       <button className={`${styles.btn} ${colorClass} ${sizeClass}`} onClick={() => action()}>{children}</button> //children.props o const {children} = a props e solo {children}
     );
 };
 
