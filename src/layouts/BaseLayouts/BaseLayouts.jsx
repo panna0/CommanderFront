587a8a5c60109e5c87564f20d8cdf2ed14e18e47
Panna0 <arianna.ferri2004@gmail.com>
2025-05-18 15:48:29 +0200

    add Header in BaseLayouts structure

--- src/layouts/BaseLayouts/BaseLayouts.jsx
+++ src/layouts/BaseLayouts/BaseLayouts.jsx
@@ -1,9 +1,25 @@
-import {Outlet} from 'react-router';
+import {Outlet, Link} from 'react-router';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+import styles from './BaseLayouts.module.scss'
+import { useNavigate } from 'react-router';
+
 
 function BaseLayouts() { //è come una classe ma è una funzione
+   const navigate = useNavigate();
+
+   
+
+
     return (
-      <div className="App">
-        <div className='container'>
+      <div>
+        <Header>
+        <div>
+         <Button color={"primary"} size={"small"} action={() => navigate('/auth/login')}>login</Button>
+
+        </div>
+        </Header>
+        <div className={`${styles.container}`}>
           <Outlet/>
         </div>
       </div>
