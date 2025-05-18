e8a7b155fc2438a6abd2315c6345892305cf0098
Panna0 <arianna.ferri2004@gmail.com>
2025-05-18 15:44:03 +0200

    add GenericImput component

---
+++ src/components/GenericInput/GenericInput.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./GenericInput.module.scss"
+
+const GenericInput = ({type, value, action, par, max, isEditable, err}) => {
+    
+   
+
+    return(
+        <div className={`${styles.GenericInputDiv}`}>
+            <label>{par}</label>
+            <input 
+            className={`${styles.GenericInput}`}
+            type={type} 
+            max={max}
+            value={value} 
+            onChange={(e) => action(e, par)}
+            readOnly={isEditable? false : true}
+            />
+            <div className={`${styles.errorDiv}`}><p>{err}</p></div>
+        </div>
+    );
+};
+
+export default GenericInput;
