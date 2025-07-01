d1ed68d9eed3fd28c49a8487379b79156c7fdf5f
Panna0 <arianna.ferri2004@gmail.com>
2025-07-01 07:55:52 +0200

    Add motion to Frame

--- src/components/Frame/Frame.jsx
+++ src/components/Frame/Frame.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./Frame.module.scss"
+import {motion} from 'motion/react'
 
 const Frame = ({children}) => {
 
     return(
-        <div className={`${styles.frameLogin}`}>
+        <motion.div 
+            initial={{ opacity: 0, scale: 0.8 }} 
+            animate={{ opacity: 1, scale: 1 }} 
+            transition={{
+            type: "spring", 
+            stiffness: 260, 
+            damping: 20,    
+            delay: 0.1,     
+        }}
+        className={`${styles.frameLogin}`}>
             <div className={`${styles.bgLogin}`}>
                 {children}
             </div>
-        </div>
+        </motion.div>
     );
 };
 
